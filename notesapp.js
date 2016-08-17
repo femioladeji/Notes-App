e813a3708824f69259652ac7a927e9aefa201d69
Oladeji Femi <femidotexe@gmail.com>
2016-08-17 13:18:58 +0100

    Made some changes and added comments

--- notesapp.js
+++ notesapp.js
@@ -1,4 +1,9 @@
-class Notes {
+class Note {
+    /**
+     * the constructor method
+     * @param noteContent a string of the noteContent
+     * @param authorName a string of the author
+     */
     constructor(noteContent, authorName){
         this.noteContent = noteContent;
         this.author = authorName;
@@ -6,11 +11,17 @@ class Notes {
 }
 
 class NotesApplication {
-    constructor(authorName) {
-        this.author = authorName;
+    /**
+     * constructor for the NotesApplication
+     */ 
+    constructor() {
         this.notes = [];
     }
     
+    /**
+     *createANote method is used to add a new note
+     * @param note is an instance of note
+     */
     createANote(note) {
         if (note instanceof Notes) {
             this.notes.push(note);
@@ -20,25 +31,38 @@ class NotesApplication {
         
     }
     
+    /**
+     * this method list all the notes. No param is required
+     */ 
     listAllNotes() {
         for(var id = 0; id < this.notes.length; id++){
-            console.log('Note ID: '+id+'\n\n'+this.notes[id].noteContent+'\n\nBy Author '+this.notes[id].author+'\n\n');
+            if(this.notes[id] !== undefined) {
+                console.log('Note ID: '+id+'\n'+this.notes[id].noteContent+'\nBy Author '+this.notes[id].author+'\n');
+            }
         }
     }
     
+    /**
+     * this method gets the string details of a particular note
+     * @oaran note_id is an integer of the 
+     */ 
     getANote(note_id) {
         if(this.notes[note_id] === undefined){
             return ('Note does not exist');
         }
-        return this.notes[note_id].noteContent;
+        return this.notes[note_id].noteContent+'\nBy'+this.notes[note_id].author;
     }
     
+    /**
+     * this function loops through the array to check if any noteContent contains a string
+     * @param searchString is the substring we are searching for
+     */ 
     searchNotes(searchString) {
         var returnString = 'Showing results for '+searchString+'\n';
         var count = 0;
         for(var id = 0; id < this.notes.length; id++) {
             if(this.notes[id].noteContent.indexOf(searchString) !== -1) {
-                returnString += 'Note ID: '+id+'\n\n'+this.notes[id].noteContent+'\n\nBy author '+this.notes[id].author+'\n';
+                returnString += 'Note ID: '+id+'\n'+this.notes[id].noteContent+'\nBy author '+this.notes[id].author+'\n';
                 count++;
             }
         }
@@ -48,6 +72,10 @@ class NotesApplication {
         return returnString;
     }
     
+    /**
+     * this method simply sets the note_id to null but checks if the id exist
+     * @param note_id is the index
+     */ 
     deleteANote(note_id) {
         if(this.notes[note_id] === undefined){
             return ('Invalid note id');
@@ -55,6 +83,11 @@ class NotesApplication {
         this.notes[note_id] = null;
     }
     
+    /** 
+     * this method is used to edit a note_id
+     * @param note_id is the index
+     * @param note_content is the new content
+     */ 
     editANote(note_id, note_content) {
         if(this.notes[note_id] === undefined){
             return ('Invalid note id');
@@ -63,15 +96,23 @@ class NotesApplication {
     }
 }
 
-var myNote = new NotesApplication('Femi Oladeji');
-var note1 = new Notes('G-Ramos has kinda been a saviour today', 'G-Ramos');
+var myNote = new NotesApplication();
+
+var note1 = new Note('G-Ramos has kinda been a saviour today', 'G-Ramos');
+
 myNote.createANote(note1);
-var note2 = new Notes('this learning is a bit intense', 'Chidiebere');
+
+var note2 = new Note('this learning is a bit intense', 'Chidiebere');
+
 myNote.createANote(note2);
 
 myNote.listAllNotes();
+
 myNote.getANote(3)
 
 console.log(myNote.searchNotes('G-Ramos'))
+
+var note3 = new Note('My TA is Toyin', 'Toyin');
+//deleteANote()
 
 
