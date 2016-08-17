a6117b5f45a2f74562ee7eb6fb5d7cc9303463d9
Oladeji Femi <femidotexe@gmail.com>
2016-08-17 12:41:54 +0100

    Created a notes class that containing the notecontent and noteauthor

--- notesapp.js
+++ notesapp.js
@@ -1,16 +1,28 @@
+class Notes {
+    constructor(noteContent, authorName){
+        this.noteContent = noteContent;
+        this.author = authorName;
+    }
+}
+
 class NotesApplication {
     constructor(authorName) {
         this.author = authorName;
         this.notes = [];
     }
     
-    createANote(note_content) {
-        this.notes.push(note_content);
+    createANote(note) {
+        if (note instanceof Notes) {
+            this.notes.push(note);
+        }else{
+            console.log('That should be an invalid note');
+        }
+        
     }
     
     listAllNotes() {
         for(var id = 0; id < this.notes.length; id++){
-            console.log('Note ID: '+id+'\n\n'+this.notes[id]+'\n\nBy Author '+this.author+'\n\n');
+            console.log('Note ID: '+id+'\n\n'+this.notes[id].noteContent+'\n\nBy Author '+this.notes[id].author+'\n\n');
         }
     }
     
@@ -18,27 +30,48 @@ class NotesApplication {
         if(this.notes[note_id] === undefined){
             return ('Note does not exist');
         }
-        return this.notes[note_id];
+        return this.notes[note_id].noteContent;
     }
     
     searchNotes(searchString) {
         var returnString = 'Showing results for '+searchString+'\n';
+        var count = 0;
         for(var id = 0; id < this.notes.length; id++) {
-            if(this.notes[id].indexOf(searchString) !== -1) {
-                returnString += 'Note ID: '+id+'\n\n'+this.notes[id]+'\n\nBy author '+this.authorName+'\n';
+            if(this.notes[id].noteContent.indexOf(searchString) !== -1) {
+                returnString += 'Note ID: '+id+'\n\n'+this.notes[id].noteContent+'\n\nBy author '+this.notes[id].author+'\n';
+                count++;
             }
         }
-        
+        if(count === 0){
+            returnString += '\n No result found for the search';
+        }
         return returnString;
+    }
+    
+    deleteANote(note_id) {
+        if(this.notes[note_id] === undefined){
+            return ('Invalid note id');
+        }
+        this.notes[note_id] = null;
+    }
+    
+    editANote(note_id, note_content) {
+        if(this.notes[note_id] === undefined){
+            return ('Invalid note id');
+        }
+        this.notes[note_id].noteContent = note_content;
     }
 }
 
 var myNote = new NotesApplication('Femi Oladeji');
-myNote.createANote('This is my first note');
-myNote.createANote('Adding another note for crying out loud');
+var note1 = new Notes('G-Ramos has kinda been a saviour today', 'G-Ramos');
+myNote.createANote(note1);
+var note2 = new Notes('this learning is a bit intense', 'Chidiebere');
+myNote.createANote(note2);
+
 myNote.listAllNotes();
 myNote.getANote(3)
 
-console.log(myNote.searchNotes('cry'))
+console.log(myNote.searchNotes('G-Ramos'))
 
 
