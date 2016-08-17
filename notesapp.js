4a07a1b838811380335a9c7abe5b27b42810ab25
Oladeji Femi <femidotexe@gmail.com>
2016-08-17 12:12:48 +0100

    Added the search method and...

--- notesapp.js
+++ notesapp.js
@@ -15,7 +15,21 @@ class NotesApplication {
     }
     
     getANote(note_id) {
+        if(this.notes[note_id] === undefined){
+            return ('Note does not exist');
+        }
         return this.notes[note_id];
+    }
+    
+    searchNotes(searchString) {
+        var returnString = 'Showing results for '+searchString+'\n';
+        for(var id = 0; id < this.notes.length; id++) {
+            if(this.notes[id].indexOf(searchString) !== -1) {
+                returnString += 'Note ID: '+id+'\n\n'+this.notes[id]+'\n\nBy author '+this.authorName+'\n';
+            }
+        }
+        
+        return returnString;
     }
 }
 
@@ -23,3 +37,8 @@ var myNote = new NotesApplication('Femi Oladeji');
 myNote.createANote('This is my first note');
 myNote.createANote('Adding another note for crying out loud');
 myNote.listAllNotes();
+myNote.getANote(3)
+
+console.log(myNote.searchNotes('cry'))
+
+
